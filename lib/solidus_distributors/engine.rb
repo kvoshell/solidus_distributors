305b4bec9cb8b9583e38845f4fafeee1e5a4b3e3
Kevin Voshell <kevin@llt-group.com>
2020-03-02 15:55:58 -0600

    add migration

--- lib/solidus_distributors/engine.rb
+++ lib/solidus_distributors/engine.rb
@@ -5,10 +5,14 @@ require 'spree/core'
 module SolidusDistributors
   class Engine < Rails::Engine
     include SolidusSupport::EngineExtensions::Decorators
-
     isolate_namespace ::Spree
-
     engine_name 'solidus_distributors'
+
+    initializer "solidus_distributor", before: :load_config_initializers do |app|
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        Rails.application.config.paths["db/migrate"] << expanded_path
+      end
+    end
 
     # use rspec for tests
     config.generators do |g|
